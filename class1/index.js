873934cdc3e3434561bbaea32cbb154f1388ebd2
asadkhan-creator <asadpm9@gmail.com>
2024-11-17 12:41:34 +0500

    Add files via upload

---
+++ class1/index.js
@@ -0,0 +1,12 @@
+const accountId=1445533
+let email="asad@gmail.com"
+var password="1234"
+city="karachi"
+
+// accountId="123456" // not allowed 
+email="dcm@gmail.com"
+password="2222"
+city="lahore"
+
+console.log(accountId);
+console.table([accountId,email,password,city])
