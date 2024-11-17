9a9ec85edd0b80c41bf76b42fc09a60f535e6234
asadkhan-creator <asadpm9@gmail.com>
2024-11-17 13:06:08 +0500

    Add files via upload

---
+++ class2/index.js
@@ -0,0 +1,28 @@
+// data types
+"use strict"; //treat all js code as newer version 
+
+// alert("hello") // we are using node js not browser
+console.log(3+3)
+console.log("asad")
+
+let name="asad"
+let age= 20
+let learningJS=true
+let state=null
+
+// premitive data types
+//number => 2^53
+// bigint
+// string =>""
+// boolean =>true and false
+// null => stand alone value
+// undefined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof "asad")
+
+console.log(typeof null)
+
+console.log(typeof undefined)
