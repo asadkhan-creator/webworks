b1ea486eced5a0b0c117641442025877eb62ad5f
asadkhan-creator <asadpm9@gmail.com>
2024-11-19 16:58:10 +0500

    Add files via upload

---
+++ class3/index.js
@@ -0,0 +1,28 @@
+// conversion of data type
+
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+//33=>33
+//33abc=>NaN
+//true=>1
+
+let isloggedin = "asad"
+let booleanisloggedin=Boolean(isloggedin)
+console.log(booleanisloggedin);
+console.log(typeof booleanisloggedin);
+
+// 0 => false 1 => true
+// asad => true
+
+let num = 123
+let string=String(num)
+console.log(string);
+console.log(typeof string);
+
